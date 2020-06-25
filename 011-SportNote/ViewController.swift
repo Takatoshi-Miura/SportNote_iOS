c1752481a441ed0712492b3c445131c08743112c
Takatoshi-Miura <takatoshi052031@gmail.com>
2020-06-25 12:39:38 +0900

    Firebase導入。

---
+++ 011-SportNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  011-SportNote
+//
+//  Created by Takatoshi Miura on 2020/06/23.
+//  Copyright © 2020 Takatoshi Miura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target '011-SportNote' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for 011-SportNote
+
+pod 'Firebase/Analytics'
+pod 'Firebase/Auth'
+pod 'Firebase/Firestore'
+pod 'SVProgressHUD'
+
+
+end
