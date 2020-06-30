e99c4ef034c5faab55744f4e05b2b332473de68c
Takatoshi-Miura <takatoshi052031@gmail.com>
2020-07-01 01:50:33 +0900

    課題画面にて、セルを右スワイプで解決済みにする機能を追加。

--- 011-SportNote/TaskViewController.swift
+++ 011-SportNote/TaskViewController.swift
@@ -21,6 +21,10 @@ class TaskViewController: UIViewController, UITableViewDelegate, UITableViewData
         // リフレッシュ機能の設定
         tableView.refreshControl = refreshCtl
         refreshCtl.addTarget(self, action: #selector(TaskViewController.refresh(sender:)), for: .valueChanged)
+        
+        // 編集ボタンの設定(複数選択可能)
+        tableView.allowsMultipleSelectionDuringEditing = true
+        navigationItem.leftBarButtonItem = editButtonItem
     }
     
     
@@ -64,24 +68,11 @@ class TaskViewController: UIViewController, UITableViewDelegate, UITableViewData
         
     }
     
-    
-    // テーブルビューを更新するメソッド
-    func reloadTableView() {
-        // データベースの課題データを取得
-        let databaseTaskData = TaskData()
-        databaseTaskData.loadUnsolvedTaskData()
-        
-        // データの取得が終わるまで時間待ち
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2.0) {
-            // 課題データの受け取り
-            self.taskDataArray = []
-            self.taskDataArray = databaseTaskData.taskDataArray
-        
-            // テーブルビューの更新
-            self.tableView?.reloadData()
-        }
+    // 編集機能
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        tableView.isEditing = editing
     }
-    
     
     // セルをタップした時の処理
     var indexPath:Int = 0   // 行番号格納用
@@ -104,7 +95,7 @@ class TaskViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     
-    // セルを削除したときの処理
+    // セルを削除したときの処理（左スワイプ）
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         // 削除処理かどうかの判定
         if editingStyle == UITableViewCell.EditingStyle.delete {
@@ -117,6 +108,51 @@ class TaskViewController: UIViewController, UITableViewDelegate, UITableViewData
             
             // セルを削除
             tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+        }
+    }
+    
+    // deleteの表示名を変更
+    func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        return "削除"
+    }
+    
+    // セルを解決済みにするときの処理（右スワイプ）
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        // アクションの定義
+        let resolveAction = UIContextualAction(style: .normal,title: "解決済み",handler: { (action: UIContextualAction, view: UIView, completion: (Bool) -> Void) in
+            // 解決済みにする
+            self.taskDataArray[indexPath.row].changeAchievement()
+            self.taskDataArray[indexPath.row].updateTaskData()
+            
+            // taskDataArrayから削除
+            self.taskDataArray.remove(at:indexPath.row)
+            
+            // セルをテーブルから削除
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.right)
+            
+            // 処理を実行完了した場合はtrue
+            completion(true)
+        })
+        resolveAction.backgroundColor = UIColor.systemBlue
+        
+        return UISwipeActionsConfiguration(actions: [resolveAction])
+    }
+    
+    
+    // テーブルビューを更新するメソッド
+    func reloadTableView() {
+        // データベースの課題データを取得
+        let databaseTaskData = TaskData()
+        databaseTaskData.loadUnsolvedTaskData()
+        
+        // データの取得が終わるまで時間待ち
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2.0) {
+            // 課題データの受け取り
+            self.taskDataArray = []
+            self.taskDataArray = databaseTaskData.taskDataArray
+        
+            // テーブルビューの更新
+            self.tableView?.reloadData()
         }
     }
     
