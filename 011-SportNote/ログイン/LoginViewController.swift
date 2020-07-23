2746bca3c71b267ffb81988a84c91d02b4b4709f
Takatoshi-Miura <takatoshi052031@gmail.com>
2020-07-23 12:45:25 +0900

    [Update] ログイン画面にて、キーボードでテキストフィールドが隠れない設定を追加。

--- 011-SportNote/ログイン/LoginViewController.swift
+++ 011-SportNote/ログイン/LoginViewController.swift
@@ -16,9 +16,14 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // ツールバーを作成
         createToolBar()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // キーボードでテキストフィールドが隠れない設定
+        configureObserver()
     }
     
     
@@ -32,7 +37,6 @@ class LoginViewController: UIViewController {
     // ログインボタンの処理
     @IBAction func loginButton(_ sender: Any) {
         if let address = mailAddressTextField.text, let password = passwordTextField.text {
-            
             // アドレスとパスワード名のいずれかでも入力されていない時は何もしない
             if address.isEmpty || password.isEmpty {
                 SVProgressHUD.showError(withStatus: "必要項目を入力して下さい")
@@ -76,7 +80,6 @@ class LoginViewController: UIViewController {
     @IBAction func createAccountButton(_ sender: Any) {
         // アドレス,パスワード名,アカウント名の入力を確認
         if let address = mailAddressTextField.text, let password = passwordTextField.text {
-            
             // アドレス,パスワード名のいずれかでも入力されていない時は何もしない
             if address.isEmpty || password.isEmpty {
                 SVProgressHUD.showError(withStatus: "必要項目を入力して下さい")
@@ -139,6 +142,34 @@ class LoginViewController: UIViewController {
     // テキストフィールド以外をタップでキーボードを下げる設定
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
+    }
+    
+    // キーボードを出したときの設定
+    func configureObserver() {
+        let notification = NotificationCenter.default
+        notification.addObserver(self, selector: #selector(keyboardWillShow(_:)),name: UIResponder.keyboardWillShowNotification, object: nil)
+        notification.addObserver(self, selector: #selector(keyboardWillHide(_:)),name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    // キーボードが表示時に画面をずらす。
+    @objc func keyboardWillShow(_ notification: Notification?) {
+        guard let rect = (notification?.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue,
+            let duration = notification?.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
+        UIView.animate(withDuration: duration) {
+            // キーボードが出ていない時は画面をずらす
+            if self.mailAddressTextField.isEditing == false || self.passwordTextField.isEditing == false {
+                let transform = CGAffineTransform(translationX: 0, y: -(rect.size.height))
+                self.view.transform = transform
+            }
+        }
+    }
+
+    // キーボードが降りたら画面を戻す
+    @objc func keyboardWillHide(_ notification: Notification?) {
+        guard let duration = notification?.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? TimeInterval else { return }
+        UIView.animate(withDuration: duration) {
+            self.view.transform = CGAffineTransform.identity
+        }
     }
     
     // ツールバーを作成するメソッド
