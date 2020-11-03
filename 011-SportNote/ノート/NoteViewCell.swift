71705359ebaf96b154637bbeebaaa223929229c2
Takatoshi Miura <takatoshi052031@gmail.com>
2020-11-04 08:01:05 +0900

    [Add] プロジェクトフォルダに、NoteViewCellを追加。

---
+++ 011-SportNote/ノート/NoteViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NoteViewCell.swift
+//  011-SportNote
+//
+//  Created by Takatoshi Miura on 2020/11/04.
+//  Copyright © 2020 Takatoshi Miura. All rights reserved.
+//
+
+import UIKit
+
+class NoteViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
