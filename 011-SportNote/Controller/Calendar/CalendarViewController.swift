fda7169ff6af79ab4449134737a5d1e4b71f96a9
Takatoshi-Miura <takatoshi052031@gmail.com>
2022-04-03 19:42:29 +0900

    [Update] カレンダー表示を最適化

--- 011-SportNote/Controller/Calendar/CalendarViewController.swift
+++ 011-SportNote/Controller/Calendar/CalendarViewController.swift
@@ -57,6 +57,85 @@ class CalendarViewController: UIViewController {
     
 }
 
+extension CalendarViewController: FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
+    
+    enum WeekDay: Int {
+        case sunday = 1
+        case monday
+        case tuesday
+        case wednesday
+        case thursday
+        case friday
+        case saturday
+    }
+    
+    /// 日付がタップされた時の処理
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let tmpDate = Calendar(identifier: .gregorian)
+        let year = tmpDate.component(.year, from: date)
+        let month = tmpDate.component(.month, from: date)
+        let day = tmpDate.component(.day, from: date)
+        print("\(year) / \(month) / \(day)")
+    }
+    
+    /// 土日や祝日の日の文字色を変える
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, titleDefaultColorFor date: Date) -> UIColor? {
+        // 祝日判定（祝日は赤色で表示する）
+        if self.judgeHoliday(date){
+            return UIColor.red
+        }
+        
+        // 土日の判定（土曜日は青色、日曜日は赤色で表示する）
+        let weekday = self.getWeekIdx(date)
+        if weekday == WeekDay.sunday.rawValue {
+            return UIColor.red
+        } else if weekday == WeekDay.saturday.rawValue {
+            return UIColor.blue
+        }
+        
+        // 曜日ラベルの色を変更
+        calendar.calendarWeekdayView.weekdayLabels[0].textColor = UIColor.red
+        calendar.calendarWeekdayView.weekdayLabels[1].textColor = UIColor.label
+        calendar.calendarWeekdayView.weekdayLabels[2].textColor = UIColor.label
+        calendar.calendarWeekdayView.weekdayLabels[3].textColor = UIColor.label
+        calendar.calendarWeekdayView.weekdayLabels[4].textColor = UIColor.label
+        calendar.calendarWeekdayView.weekdayLabels[5].textColor = UIColor.label
+        
+        // 曜日ラベルの文字列を変更
+        calendar.calendarWeekdayView.weekdayLabels[0].text = "Sun"
+        calendar.calendarWeekdayView.weekdayLabels[1].text = "Mon"
+        calendar.calendarWeekdayView.weekdayLabels[2].text = "Tue"
+        calendar.calendarWeekdayView.weekdayLabels[3].text = "Wed"
+        calendar.calendarWeekdayView.weekdayLabels[4].text = "Thu"
+        calendar.calendarWeekdayView.weekdayLabels[5].text = "Fri"
+        calendar.calendarWeekdayView.weekdayLabels[6].text = "Sat"
+        
+        return nil
+    }
+    
+    /// 祝日判定
+    /// - Parameters:
+    ///     - date: 日付
+    /// - Returns: true→祝日、false→祝日以外
+    func judgeHoliday(_ date : Date) -> Bool {
+        let tmpCalendar = Calendar(identifier: .gregorian)
+        let year = tmpCalendar.component(.year, from: date)
+        let month = tmpCalendar.component(.month, from: date)
+        let day = tmpCalendar.component(.day, from: date)
+        let holiday = CalculateCalendarLogic()
+        return holiday.judgeJapaneseHoliday(year: year, month: month, day: day)
+    }
+    
+    /// 曜日判定
+    /// - Parameters:
+    ///     - date: 日付
+    /// - Returns: 曜日番号
+    func getWeekIdx(_ date: Date) -> Int{
+        let tmpCalendar = Calendar(identifier: .gregorian)
+        return tmpCalendar.component(.weekday, from: date)
+    }
+}
+
 extension CalendarViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
