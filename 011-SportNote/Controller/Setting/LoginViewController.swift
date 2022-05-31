519ebceca3752d5c95444db954683c80e4f12813
Takatoshi-Miura <takatoshi052031@gmail.com>
2022-06-01 08:01:53 +0900

    [Add] ログイン画面初期化処を追加

--- 011-SportNote/Controller/Setting/LoginViewController.swift
+++ 011-SportNote/Controller/Setting/LoginViewController.swift
@@ -23,16 +23,53 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordChangeButton: UIButton!
     @IBOutlet weak var createAccountButton: UIButton!
     @IBOutlet weak var cancelButton: UIButton!
+    private var isLogin: Bool = false
     var delegate: LoginViewControllerDelegate?
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        // ログイン状態の判定
+        if let _ = UserDefaults.standard.object(forKey: "address") as? String,
+           let _ = UserDefaults.standard.object(forKey: "password") as? String
+        {
+            isLogin = true
+        }
+        initView()
+    }
+    
+    ///画面初期化
+    func initView() {
+        loginTextField.placeholder = TITLE_MAIL_ADDRESS
+        passwordTextField.placeholder = TITLE_PASSWORD
+        if isLogin {
+            label.text = TITLE_ALREADY_LOGIN
+            loginButton.backgroundColor = UIColor.systemRed
+            loginButton.setTitle(TITLE_LOGOUT, for: .normal)
+            if let address = UserDefaults.standard.object(forKey: "address") as? String {
+                loginTextField.text = address
+            }
+            if let password = UserDefaults.standard.object(forKey: "password") as? String {
+                passwordTextField.text = password
+            }
+        } else {
+            label.text = TITLE_LOGIN_HELP
+            loginButton.backgroundColor = UIColor.systemTeal
+            loginButton.setTitle(TITLE_LOGIN, for: .normal)
+        }
+        passwordChangeButton.setTitle(TITLE_PASSWORD_RESET, for: .normal)
+        createAccountButton.setTitle(TITLE_CREATE_ACCOUNT, for: .normal)
+        cancelButton.setTitle(TITLE_CANCEL, for: .normal)
     }
     
     // MARK: - Action
     
     @IBAction func tapLoginButton(_ sender: Any) {
+        if isLogin {
+            // TODO: ログアウト処理
+        } else {
+            // TODO: ログイン処理
+        }
     }
     
     @IBAction func tapPasswordChangeButton(_ sender: Any) {
