06b180d08cb6eb6dc98f5a2d06161b85eaddb1b2
Takatoshi-Miura <takatoshi052031@gmail.com>
2024-06-19 07:46:11 +0900

    [Add] Swift Testing導入

---
+++ SportsNoteTests/SportsNoteTests.swift
@@ -0,0 +1,33 @@
+//
+//  SportsNoteTests.swift
+//  SportsNoteTests
+//
+//  Created by Takatoshi Miura on 2024/06/19.
+//  Copyright © 2024 Takatoshi Miura. All rights reserved.
+//
+
+@testable import _11_SportNote
+import Testing
+
+extension Tag {
+    @Tag static var util: Self
+}
+
+struct SportsNoteTests {
+    
+    @Suite(.tags(.util))
+    struct AppInfoTests {
+        @Test func appVersion() async throws {
+            let appVersion = getAppVersion()
+            let appVersionStr = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+            #expect(appVersion == appVersionStr)
+        }
+        
+        @Test func buildNo() async throws {
+            let buildNo = getBuildNo()
+            let buildNoStr = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String
+            #expect(buildNo == buildNoStr)
+        }
+    }
+
+}
