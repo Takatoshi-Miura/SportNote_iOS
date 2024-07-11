436d05dc0ab5df61f771e238ce6a81fe47f07938
Takatoshi-Miura <takatoshi052031@gmail.com>
2024-07-11 09:25:52 +0900

    [Update] Swift Testing無効化

--- SportsNoteTests/SportsNoteTests.swift
+++ SportsNoteTests/SportsNoteTests.swift
@@ -6,28 +6,28 @@
 //  Copyright © 2024 Takatoshi Miura. All rights reserved.
 //
 
-@testable import _11_SportNote
-import Testing
-
-extension Tag {
-    @Tag static var util: Self
-}
-
-struct SportsNoteTests {
-    
-    @Suite(.tags(.util))
-    struct AppInfoTests {
-        @Test func appVersion() async throws {
-            let appVersion = getAppVersion()
-            let appVersionStr = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
-            #expect(appVersion == appVersionStr)
-        }
-        
-        @Test func buildNo() async throws {
-            let buildNo = getBuildNo()
-            let buildNoStr = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String
-            #expect(buildNo == buildNoStr)
-        }
-    }
-
-}
+//@testable import _11_SportNote
+//import Testing
+//
+//extension Tag {
+//    @Tag static var util: Self
+//}
+//
+//struct SportsNoteTests {
+//    
+//    @Suite(.tags(.util))
+//    struct AppInfoTests {
+//        @Test func appVersion() async throws {
+//            let appVersion = getAppVersion()
+//            let appVersionStr = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+//            #expect(appVersion == appVersionStr)
+//        }
+//        
+//        @Test func buildNo() async throws {
+//            let buildNo = getBuildNo()
+//            let buildNoStr = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String
+//            #expect(buildNo == buildNoStr)
+//        }
+//    }
+//
+//}
