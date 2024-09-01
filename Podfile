543591dbde9b1153cb2c6bb60f3a3cca2d5bb6f5
Takatoshi-Miura <takatoshi052031@gmail.com>
2024-09-01 17:17:28 +0900

    [Add] Realm(Group)のテストを追加

--- Podfile
+++ Podfile
@@ -3,6 +3,17 @@ platform :ios, '14.0'
 
 post_install do |installer|
   installer.pods_project.targets.each do |target|
+    
+    if target.name == 'BoringSSL-GRPC'
+      target.source_build_phase.files.each do |file|
+        if file.settings && file.settings['COMPILER_FLAGS']
+          flags = file.settings['COMPILER_FLAGS'].split
+          flags.reject! { |flag| flag == '-GCC_WARN_INHIBIT_ALL_WARNINGS' }
+          file.settings['COMPILER_FLAGS'] = flags.join(' ')
+        end
+      end
+    end
+    
     target.build_configurations.each do |config|
       config.build_settings["EXCLUDED_ARCHS[sdk=iphonesimulator*]"] = "arm64"
       config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '12.0'
--- SportsNoteTests/RealmManagerTests.swift
+++ SportsNoteTests/RealmManagerTests.swift
@@ -9,8 +9,60 @@
 @testable import _11_SportNote
 import Testing
 import Foundation
+import RealmSwift
 
-@Suite(.tags(.realm))
-struct RealmManagerTests {
+final class RealmManagerTests {
+    
+    var realm: Realm!
+    var originalDefaultConfiguration: Realm.Configuration!
+    var realmManager: RealmManager!
+    
+    init() {
+        // 既存のdefaultConfigurationを保存
+        originalDefaultConfiguration = Realm.Configuration.defaultConfiguration
+        
+        // 他の環境に影響を与えないように、テスト用のインメモリRealmの設定をdefaultConfigurationに適用
+        let config = Realm.Configuration(inMemoryIdentifier: "RealmManagerTests")
+        Realm.Configuration.defaultConfiguration = config
+        realm = try! Realm()
+        realmManager = RealmManager()
+        
+        // テストデータを挿入
+        createTestGroup()
+    }
+    
+    deinit {
+        // テスト終了後、デフォルトの設定を元に戻す
+        Realm.Configuration.defaultConfiguration = originalDefaultConfiguration
+        
+        // テスト終了後にデータをクリーンアップ
+        try! realm.write {
+            realm.deleteAll()
+        }
+        realm = nil
+        realmManager = nil
+    }
+    
+    /// テスト用のGroupデータを作成
+    private func createTestGroup() {
+        let group1 = Group(title: "赤グループ", color: .red, order: 1)
+        let group2 = Group(title: "青グループ", color: .blue, order: 2)
+        
+        try! realm.write {
+            realm.add(group1)
+            realm.add(group2)
+        }
+    }
+    
+    // MARK: - Group
+    
+    @Test("Groupを全取得")
+    func testGetAllGroup() {
+        let result = realmManager.getAllGroup()
+        let expectedCount = 2
+        #expect(result.count == expectedCount)
+        #expect(result[0].title == "赤グループ")
+        #expect(result[1].title == "青グループ")
+    }
     
 }
